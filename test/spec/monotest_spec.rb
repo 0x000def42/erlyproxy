c82c00a824d98cb4dfb2a24c9b65aee75f666cf6
Dmitriy Vishnevskiy <0x000def42@gmail.com>
2021-01-06 05:46:39 +0300

    Add ws to amqp test

--- test/spec/monotest_spec.rb
+++ test/spec/monotest_spec.rb
@@ -2,6 +2,7 @@
 
 require 'amqp'
 require 'websocket-eventmachine-client'
+require 'json'
 
 require_relative 'monotest_helper'
 class Monotest < MonotestHelper::Async
@@ -18,8 +19,31 @@ class Monotest < MonotestHelper::Async
       end
     end
     ws.onerror do |error|
-      p error
       failture.call error
+    end
+  end
+
+  scenario 'Passing message from ws to amqp' do |success, failture|
+    EventMachine::Timer.new(1) do
+      # failture.call 'Timeout'
+    end
+    connection = AMQP.connect(host: 'localhost')
+    ch  = AMQP::Channel.new(connection)
+    q   = ch.queue('host_request', auto_delete: true)
+
+    q.subscribe do |_metadata, payload|
+      data = JSON.parse(payload)
+      if data['payload'] == 'any' && !data['id'].nil?
+        success.call
+      else
+        failture.call "Unexpected payload: #{payload}"
+      end
+    end
+
+    ws = WebSocket::EventMachine::Client.connect(uri: 'ws://localhost:8080')
+    ws.onopen do
+      request = { target: 'host', action: 'request', payload: 'any' }
+      ws.send(JSON.generate(request))
     end
   end
 end
